416cd48591b107d95731d565cabd95820abd4486
Jorge Cobos <cobos1932@gmail.com>
2023-09-23 16:38:35 -0600

    day 3

--- firstJunitTest/src/main/java/org/example/Main.java
+++ firstJunitTest/src/main/java/org/example/Main.java
@@ -64,5 +64,15 @@ public class Main {
     }
 
     System.out.println("Largest Number: " + largest);
-  }
+  
+    }
+      String onum = "157";
+ 
+        // octal to decimal using Integer.parseInt()
+        int num = Integer.parseInt(onum, 8);
+ 
+        System.out.println(
+            "Decimal equivalent of Octal value 157 is: "
+            + num);
+    }
 }
