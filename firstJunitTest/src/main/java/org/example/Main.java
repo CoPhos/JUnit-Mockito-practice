a852fe1c265ad6718103e785ed2272f13acb53c5
Jorge Cobos <cobos1932@gmail.com>
2023-10-08 16:06:41 -0600

    day6

--- firstJunitTest/src/main/java/org/example/Main.java
+++ firstJunitTest/src/main/java/org/example/Main.java
@@ -95,5 +95,16 @@ public class Main {
         }
 
         return -1;
-    }
+    try {
+      File myObj = new File("filename.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }}
 }
