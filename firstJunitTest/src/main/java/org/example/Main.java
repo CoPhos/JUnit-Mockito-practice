10d3cb7d25693510e107db083d8ee1a7409571cd
Jorge Cobos <cobos1932@gmail.com>
2023-10-09 11:50:30 -0600

    day6

--- firstJunitTest/src/main/java/org/example/Main.java
+++ firstJunitTest/src/main/java/org/example/Main.java
@@ -106,5 +106,23 @@ public class Main {
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
-    }}
+    }
+	}
+     public static int amount = 0;
+
+  public static void main(String[] args) {
+    Main thread = new Main();
+    thread.start();
+    // Wait for the thread to finish
+    while(thread.isAlive()) {
+    System.out.println("Waiting...");
+  }
+  // Update amount and print its value
+  System.out.println("Main: " + amount);
+  amount++;
+  System.out.println("Main: " + amount);
+  }
+  public void run() {
+    amount++;
+  }
 }
