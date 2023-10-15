e12fb833ce945ed39b3c976b6aa2f82fe1a1b120
Jorge Cobos <cobos1932@gmail.com>
2023-10-15 16:35:11 -0600

    day 8

--- firstJunitTest/src/main/java/org/example/Main.java
+++ firstJunitTest/src/main/java/org/example/Main.java
@@ -139,4 +139,16 @@ public class Main {
     while(it.hasNext()) {
       System.out.println(it.next());
     }
+    int n1 = 366, n2 = 60;
+    int hcf = hcf(n1, n2);
+
+    System.out.printf("G.C.D of %d and %d is %d.", n1, n2, hcf);
 }
+
+    public static int hcf(int n1, int n2)
+    {
+        if (n2 != 0)
+            return hcf(n2, n1 % n2);
+        else
+            return n1;
+    }
