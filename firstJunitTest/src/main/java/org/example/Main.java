21b28c3f381e8ed61d1bb8762cf5aa163b271826
Jorge Cobos <cobos1932@gmail.com>
2023-11-19 15:06:19 -0600

    day 16

--- firstJunitTest/src/main/java/org/example/Main.java
+++ firstJunitTest/src/main/java/org/example/Main.java
@@ -1,220 +1,165 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-	int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90};
-    int ele = 60;
-
-    int foundIndex = jumpSearch(arr, ele);
-    System.out.println(foundIndex > 0 ? "Found at index : " + foundIndex : "Element Not Found");
-
-    Main thread = new Main();
-    thread.start();
-    // Wait for the thread to finish
-    while(thread.isAlive()) {
-    System.out.println("Waiting..."); 
-  
-  // Update amount and print its value
-  System.out.println("Main: " + amount);
-  amount++;
-  System.out.println("Main: " + amount);
-
-
-     //Initialize array  
-        int [] arr = new int [] {25, 11, 7, 75, 56};  
-        //Initialize min with first element of array.  
-        int min = arr[0];  
-        //Loop through the array  
-        for (int i = 0; i < arr.length; i++) {  
-            //Compare elements of array with min  
-           if(arr[i] <min)  
-               min = arr[i];  
-        }  
-        System.out.println("Smallest element present in given array: " + min);  
-  }
-
-  public static int jumpSearch(int[] arr, int ele) {
-
-    int prev = 0;
-    int n = arr.length;
-    int step = (int) Math.floor(Math.sqrt(n));
-
-    //loop until current element is less than the given search element
-    while (arr[Math.min(step, n) - 1] < ele) {
-      prev = step;
-      step += (int) Math.floor(Math.sqrt(n));
-      if (prev >= n) return -1;
-    }
-
-    //perform linear search prev index element to given element
-    while (arr[prev] < ele) {
-      prev++;
-      if (prev == Math.min(step, n)) return -1;
-    }
-
-    // Return index if element is found
-    if (arr[prev] == ele) return prev;
-
-    return -1;
-    }
-
-
-    // declaring double type variables
-    Double n1 = -1.0, n2 = 4.5, n3 = -5.3, largest;
-
-    // checks if n1 is greater than or equal to n2
-    if (n1 >= n2) {
-
-      // if...else statement inside the if block
-      // checks if n1 is greater than or equal to n3
-      if (n1 >= n3) {
-        largest = n1;
-      }
-
-      else {
-        largest = n3;
-      }
-    } else {
-
-      // if..else statement inside else block
-      // checks if n2 is greater than or equal to n3
-      if (n2 >= n3) {
-        largest = n2;
-      }
-
-      else {
-        largest = n3;
-      }
-    }
-
-    System.out.println("Largest Number: " + largest);
-  
-    }
-      String onum = "157";
- 
-        // octal to decimal using Integer.parseInt()
-        int num = Integer.parseInt(onum, 8);
- 
-        System.out.println(
-            "Decimal equivalent of Octal value 157 is: "
-            + num);
-     // Creating an ArrayList
-        ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-
-        // Using min()
-        int min = Collections.min(numbers);
-        System.out.println("Minimum Element: " + min);
-
-        // Using max()
-        int max = Collections.max(numbers);
-        System.out.println("Maximum Element: " + max);}
-	public static int linearSearch(int[] array, int searched) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == searched) {
-                return i;
-            }
-        }
-
-        return -1;
-    try {
-      File myObj = new File("filename.txt");
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
-      }
-      myReader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
-	}
-     public static int amount = 0;
-
-  public void run() {
-    amount++;
-  }
-   // Make a collection
-    ArrayList<String> cars = new ArrayList<String>();
-    cars.add("Volvo");
-    cars.add("BMW");
-    cars.add("Ford");
-    cars.add("Mazda");
-
-    // Get the iterator
-    Iterator<String> it = cars.iterator();
-
-    // Loop through a collection
-    while(it.hasNext()) {
-      System.out.println(it.next());
-    }
-    int n1 = 366, n2 = 60;
-    int hcf = hcf(n1, n2);
-
-    System.out.printf("G.C.D of %d and %d is %d.", n1, n2, hcf);
-    // value a, b, and c
-    double a = 2.3, b = 4, c = 5.6;
-    double root1, root2;
-
-    // calculate the determinant (b2 - 4ac)
-    double determinant = b * b - 4 * a * c;
-
-    // check if determinant is greater than 0
-    if (determinant > 0) {
-
-      // two real and distinct roots
-      root1 = (-b + Math.sqrt(determinant)) / (2 * a);
-      root2 = (-b - Math.sqrt(determinant)) / (2 * a);
-
-      System.out.format("root1 = %.2f and root2 = %.2f", root1, root2);
-    }
-
-    // check if determinant is equal to 0
-    else if (determinant == 0) {
-
-      // two real and equal roots
-      // determinant is equal to 0
-      // so -b + 0 == -b
-      root1 = root2 = -b / (2 * a);
-      System.out.format("root1 = root2 = %.2f;", root1);
-    }
-
-    // if determinant is less than zero
-    else {
-
-      // roots are complex number and distinct
-      double real = -b / (2 * a);
-      double imaginary = Math.sqrt(-determinant) / (2 * a);
-      System.out.format("root1 = %.2f+%.2fi", real, imaginary);
-      System.out.format("\nroot2 = %.2f-%.2fi", real, imaginary);
-    }
-  }
-int decimal = 78;
-        int octal = convertDecimalToOctal(decimal);
-        System.out.printf("%d in decimal = %d in octal", decimal, octal);
     
+    //Represent a node of binary tree  
+    public static class Node{  
+        int data;  
+        Node left;  
+        Node right;  
+  
+        public Node(int data){  
+            //Assign data to the new node, set left and right children to null  
+            this.data = data;  
+            this.left = null;  
+            this.right = null;  
+        }  
+      }  
+  
+      //Represent the root of binary tree  
+      public Node root;  
+  
+      public BinarySearchTree(){  
+          root = null;  
+      }  
+  
+      //insert() will add new node to the binary search tree  
+      public void insert(int data) {  
+          //Create a new node  
+          Node newNode = new Node(data);  
+  
+          //Check whether tree is empty  
+          if(root == null){  
+              root = newNode;  
+              return;  
+            }  
+          else {  
+              //current node point to root of the tree  
+              Node current = root, parent = null;  
+  
+              while(true) {  
+                  //parent keep track of the parent node of current node.  
+                  parent = current;  
+  
+                  //If data is less than current's data, node will be inserted to the left of tree  
+                  if(data < current.data) {  
+                      current = current.left;  
+                      if(current == null) {  
+                          parent.left = newNode;  
+                          return;  
+                      }  
+                  }  
+                  //If data is greater than current's data, node will be inserted to the right of tree  
+                  else {  
+                      current = current.right;  
+                      if(current == null) {  
+                          parent.right = newNode;  
+                          return;  
+                      }  
+                  }  
+              }  
+          }  
+      }  
+  
+      //minNode() will find out the minimum node  
+      public Node minNode(Node root) {  
+          if (root.left != null)  
+              return minNode(root.left);  
+          else  
+              return root;  
+      }  
+  
+      //deleteNode() will delete the given node from the binary search tree  
+      public Node deleteNode(Node node, int value) {  
+          if(node == null){  
+              return null;  
+           }  
+          else {  
+              //value is less than node's data then, search the value in left subtree  
+              if(value < node.data)  
+                  node.left = deleteNode(node.left, value);  
+  
+              //value is greater than node's data then, search the value in right subtree  
+              else if(value > node.data)  
+                  node.right = deleteNode(node.right, value);  
+  
+              //If value is equal to node's data that is, we have found the node to be deleted  
+              else {  
+                  //If node to be deleted has no child then, set the node to null  
+                  if(node.left == null && node.right == null)  
+                      node = null;  
+  
+                  //If node to be deleted has only one right child  
+                  else if(node.left == null) {  
+                      node = node.right;  
+                  }  
+  
+                  //If node to be deleted has only one left child  
+                  else if(node.right == null) {  
+                      node = node.left;  
+                  }  
+  
+                  //If node to be deleted has two children node  
+                  else {  
+                      //then find the minimum node from right subtree  
+                      Node temp = minNode(node.right);  
+                      //Exchange the data between node and temp  
+                      node.data = temp.data;  
+                      //Delete the node duplicate node from right subtree  
+                      node.right = deleteNode(node.right, temp.data);  
+                  }  
+              }  
+              return node;  
+          }  
+      }  
+  
+      //inorder() will perform inorder traversal on binary search tree  
+      public void inorderTraversal(Node node) {  
+  
+          //Check whether tree is empty  
+          if(root == null){  
+              System.out.println("Tree is empty");  
+              return;  
+           }  
+          else {  
+  
+              if(node.left!= null)  
+                  inorderTraversal(node.left);  
+              System.out.print(node.data + " ");  
+              if(node.right!= null)  
+                  inorderTraversal(node.right);  
+  
+          }  
+      }  
+
+    public static void main(String[] args) {
+        BinarySearchTree bt = new BinarySearchTree();  
+          //Add nodes to the binary tree  
+          bt.insert(50);  
+          bt.insert(30);  
+          bt.insert(70);  
+          bt.insert(60);  
+          bt.insert(10);  
+          bt.insert(90);  
+  
+          System.out.println("Binary search tree after insertion:");  
+          //Displays the binary tree  
+          bt.inorderTraversal(bt.root);  
+  
+          Node deletedNode = null;  
+          //Deletes node 90 which has no child  
+          deletedNode = bt.deleteNode(bt.root, 90);  
+          System.out.println("\nBinary search tree after deleting node 90:");  
+          bt.inorderTraversal(bt.root);  
+  
+          //Deletes node 30 which has one child  
+          deletedNode = bt.deleteNode(bt.root, 30);  
+          System.out.println("\nBinary search tree after deleting node 30:");  
+          bt.inorderTraversal(bt.root);  
+  
+          //Deletes node 50 which has two children  
+          deletedNode = bt.deleteNode(bt.root, 50);  
+          System.out.println("\nBinary search tree after deleting node 50:");  
+          bt.inorderTraversal(bt.root);  
+  }
 }
-public static int convertDecimalToOctal(int decimal)
-    {
-        int octalNumber = 0, i = 1;
-
-        while (decimal != 0)
-        {
-            octalNumber += (decimal % 8) * i;
-            decimal /= 8;
-            i *= 10;
-        }
-
-        return octalNumber;
-    }
-
-    public static int hcf(int n1, int n2)
-    {
-        if (n2 != 0)
-            return hcf(n2, n1 % n2);
-        else
-            return n1;
-    }
