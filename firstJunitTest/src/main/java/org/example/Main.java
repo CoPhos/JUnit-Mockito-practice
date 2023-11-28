5926c41354332dc278f137fe7f396dc3b0b9a87a
Jorge Cobos <cobos1932@gmail.com>
2023-11-27 18:24:55 -0600

    day 18

--- firstJunitTest/src/main/java/org/example/Main.java
+++ firstJunitTest/src/main/java/org/example/Main.java
@@ -211,6 +211,25 @@ public class Main {
         System.out.println("Not found");
         else
         System.out.println("Element found at index " + result);
+
+ // create first set
+    Set<Integer> numbers = new HashSet<>();
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+    numbers.add(4);
+    System.out.println("Numbers: " + numbers);
+
+    // create second set
+    Set<Integer> primeNumbers = new HashSet<>();
+    primeNumbers.add(2);
+    primeNumbers.add(3);
+    System.out.println("Prime Numbers: " + primeNumbers);
+
+    // Difference between HashSet1 and HashSet2
+    numbers.removeAll(primeNumbers);
+    System.out.println("Numbers without prime numbers: " + numbers);
+    }
     }
   }
 }
