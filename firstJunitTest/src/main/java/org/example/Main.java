2d10ba8070543237bd3491a73beb610e4f6cb828
Jorge Cobos <cobos1932@gmail.com>
2023-12-23 18:34:03 -0600

    day 23

--- firstJunitTest/src/main/java/org/example/Main.java
+++ firstJunitTest/src/main/java/org/example/Main.java
@@ -345,5 +345,46 @@ public class Main {
         else {
         System.out.println("File doesn't exit");
         }
+        File file = new File("Test.java");
+
+        
+        String fileName = file.toString();
+
+        int index = fileName.lastIndexOf('.');
+        if(index > 0) {
+        String extension = fileName.substring(index + 1);
+        System.out.println("File extension is " + extension);
+        }
+         File directory = new File("Directory");
+
+        
+        File[] files = directory.listFiles();
+        System.out.println("Files\t\t\tExtension");
+
+        for(File file : files) {
+        
+        String fileName = file.toString();
+
+        int index = fileName.lastIndexOf('.');
+        if(index > 0) {
+            String extension = fileName.substring(index + 1);
+            System.out.println(fileName + "\t" + extension);
+        }
+        
+        File file = new File("C:\\Users\\Sudip Bhandari\\Desktop\\Programiz\\Java Article\\Test.class");
+
+        String fileName = file.getName();
+        System.out.println("File Name: " + fileName);
+        File file = new File("C:\\Users\\Sudip Bhandari\\Desktop\\Programiz\\Java Article\\Test.class");
+
+    
+        String stringFile = file.toString();
+
+        int index = stringFile.lastIndexOf('\\');
+        if(index > 0) {
+            String fileName = stringFile.substring(index + 1);
+            System.out.println("File Name: " + fileName);
+        }
+
     }
   }
